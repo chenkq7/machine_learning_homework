0392bfd2c107a403d3c4cf73bdb5c11e6315ae28
cheney <17231055@buaa.edu.cn>
2020-12-21 12:01:25 +0800

    done: select model(). find: Top-k model all use random_forest model.

--- cost.py
+++ cost.py
@@ -1,43 +1,84 @@
 import pandas as pd
 import numpy as np
 
-raw_train_data = pd.read_csv("dataset/cost/train.csv")
 
-nominal_columns = ['sex', 'smoker', 'region']
-ratio_columns = ['children', 'age', 'bmi']
-feature_columns = nominal_columns + ratio_columns
-label_columns = ['charges']
-
-if __name__ == '__main__':
-    # dataset independent preprocess
-    from sklearn.preprocessing import OrdinalEncoder, OneHotEncoder
-    oe = OrdinalEncoder()
-    nominal_encoded = oe.fit_transform(raw_train_data[nominal_columns])
-    X = np.column_stack((raw_train_data[ratio_columns], nominal_encoded))
-    y = raw_train_data[label_columns]
+def select_model():
+    """
+    模型初选. 观察score前n名的params, 可以发现这些模型均使用了random_forest模型.
+    :return 网格搜索结果 grid
+    """
+    # read data
+    raw_data = pd.read_csv("dataset/cost/train.csv")
+    labels = ['charges']
+    X, y = raw_data.drop(labels, axis=1), raw_data[labels]
 
     # train val split
     from sklearn.model_selection import train_test_split
+
     X_train, X_test, y_train, y_test = train_test_split(X, y)
 
-    # dataset dependent preprocess
-    from sklearn.preprocessing import StandardScaler
-    from sklearn.pipeline import make_pipeline
+    # processor
+    from sklearn.compose import make_column_transformer, make_column_selector
+    from sklearn.preprocessing import OrdinalEncoder, OneHotEncoder, StandardScaler
+    from sklearn.preprocessing import QuantileTransformer
 
-    # model select
-    from sklearn import linear_model
+    processor_lin = make_column_transformer(
+        (OneHotEncoder(), make_column_selector(dtype_include=object)),
+        (StandardScaler(), make_column_selector(dtype_include=np.number)),
+        remainder='passthrough')
+    processor_nlin = make_column_transformer(
+        (OrdinalEncoder(), make_column_selector(dtype_include=object)),
+        (StandardScaler(), make_column_selector(dtype_include=np.number)),
+        remainder='passthrough')
+
+    # target_transformers
+    from sklearn.preprocessing import FunctionTransformer
+
+    target_transformers = [
+        None,  # None means identical transform
+        QuantileTransformer(n_quantiles=300, output_distribution="normal"),
+        FunctionTransformer(func=np.log, inverse_func=np.exp),
+        FunctionTransformer(func=np.log1p, inverse_func=np.expm1),
+    ]
+
+    # model
     from sklearn.ensemble import RandomForestRegressor
-    # svr = make_pipeline(StandardScaler(), linear_model.ElasticNet(alpha=0.1, l1_ratio=0.7))
-    svr = make_pipeline(StandardScaler(), RandomForestRegressor())
+    from sklearn.linear_model import LinearRegression, Ridge
+    from sklearn.svm import SVR
 
-    # fit
-    svr.fit(X_train, y_train)
+    models = [
+        RandomForestRegressor(),
+        LinearRegression(),
+        Ridge(),
+        SVR(),
+    ]
 
-    # metrics
-    from sklearn.metrics import r2_score
-    score = svr.score(X_test, y_test)
-    pred = svr.predict(X_test)
-    print(pred[:10])
-    print(y_test[:10])
+    # pipe together
+    from sklearn.pipeline import Pipeline
+    from sklearn.compose import TransformedTargetRegressor
+
+    tt = TransformedTargetRegressor(regressor=SVR(), transformer=None)
+    pipe = Pipeline([
+        ('processor', processor_lin),
+        ('ttr', tt),
+    ])
+
+    # grid search CV fit
+    from sklearn.model_selection import GridSearchCV
+
+    param_grid = dict(
+        processor=[processor_lin, processor_nlin],
+        ttr__transformer=target_transformers,
+        ttr__regressor=models,
+    )
+
+    grid = GridSearchCV(pipe, n_jobs=1, param_grid=param_grid)
+    grid.fit(X_train, y_train)
+    score = grid.score(X_test, y_test)
     print(score)
-    print(r2_score(y_test, pred))
+    print(grid.best_params_)
+    return grid
+
+
+if __name__ == '__main__':
+    grid = select_model()
